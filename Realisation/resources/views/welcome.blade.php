dfddd4bdf3d874855aef6c6c6dee2f390f0c6ded
stitou-nada <nadastito1087@gmail.com>
2022-10-25 11:09:19 +0100

    input

--- Realisation/resources/views/welcome.blade.php
+++ Realisation/resources/views/welcome.blade.php
@@ -1,11 +1,12 @@
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<title>Bootstrap CRUD Data Table for Database with Modal Form</title>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <title>Bootstrap CRUD Data Table for Database with Modal Form</title>
 <link rel="stylesheet" href="{{asset('css/header.css')}}">
+<link rel="stylesheet" href="{{asset("css/main.css")}}">
 <link rel="stylesheet" href="{{asset('css/table.css')}}">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round">
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
@@ -44,85 +45,53 @@
 </header>
 
 
-<aside class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3   bg-gradient-dark" id="sidenav-main" style=" height: 500px; width: 500px; background-image: linear-gradient(195deg, #6632b1 0%, #191919 100%); ">
-    <div class="sidenav-header">
-      <i class="fas fa-times p-3 cursor-pointer text-white opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
-      <a class="navbar-brand m-0" href=" https://demos.creative-tim.com/material-dashboard/pages/dashboard " target="_blank">
-        <span class="ms-1 font-weight-bold text-white">Material Dashboard 2</span>
-      </a>
-    </div>
-    <hr class="horizontal light mt-0 mb-2">
-    <div class="collapse navbar-collapse  w-auto  max-height-vh-100" id="sidenav-collapse-main">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link text-white active bg-gradient-primary" href="../pages/dashboard.html">
-            <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
-              <i class="material-icons opacity-10">dashboard</i>
-            </div>
-            <span class="nav-link-text ms-1">Dashboard</span>
+
+<div class="container-xl tableBody ">
+    <aside class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3   bg-gradient-dark
+    " id="sidenav-main" >
+        <div class="sidenav-header">
+          <i class="fas fa-times p-3 cursor-pointer text-white opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
+          <a class="navbar-brand m-0" href=" https://demos.creative-tim.com/material-dashboard/pages/dashboard " target="_blank">
+            <span class="ms-1 font-weight-bold text-white">Material Dashboard 2</span>
           </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link text-white " href="../pages/tables.html">
-            <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
-              <i class="material-icons opacity-10">table_view</i>
-            </div>
-            <span class="nav-link-text ms-1">Tables</span>
-          </a>
-        </li>
-        
-        
-        <li class="nav-item">
-          <a class="nav-link text-white " href="../pages/rtl.html">
-            <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
-              <i class="material-icons opacity-10">format_textdirection_r_to_l</i>
-            </div>
-            <span class="nav-link-text ms-1">RTL</span>
-          </a>
-        </li>
-      
-        <li class="nav-item mt-3">
-          <h6 class="ps-4 ms-2 text-uppercase text-xs text-white font-weight-bolder opacity-8">Account pages</h6>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link text-white " href="../pages/profile.html">
-            <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
-              <i class="material-icons opacity-10">person</i>
-            </div>
-            <span class="nav-link-text ms-1">Profile</span>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link text-white " href="../pages/sign-in.html">
-            <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
-              <i class="material-icons opacity-10">login</i>
-            </div>
-            <span class="nav-link-text ms-1">Sign In</span>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link text-white " href="../pages/sign-up.html">
-            <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
-              <i class="material-icons opacity-10">assignment</i>
-            </div>
-            <span class="nav-link-text ms-1">Sign Up</span>
-          </a>
-        </li>
-      </ul>
-    </div>
-    <div class="sidenav-footer position-absolute w-100 bottom-0 ">
-      <div class="mx-3">
-        <a class="btn bg-gradient-primary mt-4 w-100" href="https://www.creative-tim.com/product/material-dashboard-pro?ref=sidebarfree" type="button">Upgrade to pro</a>
-      </div>
-    </div>
-  </aside>
+        </div>
+        <hr class="horizontal light mt-0 mb-2">
+        <div class="collapse navbar-collapse  w-auto  max-height-vh-100" id="sidenav-collapse-main">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link text-white active bg-gradient-primary" href="../pages/dashboard.html">
+                <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+                  <i class="material-icons opacity-10">dashboard</i>
+                </div>
+                <span class="nav-link-text ms-1">Dashboard</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-white " href="../pages/tables.html">
+                <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+                  <i class="material-icons opacity-10">table_view</i>
+                </div>
+                <span class="nav-link-text ms-1">Tables</span>
+              </a>
+            </li>
 
 
-<div class="container-xl tableBody">
+
+
+
+
+
+          </ul>
+        </div>
+
+      </aside>
+
+
+
+
+
 	<div class="table-responsive">
 		<div class="table-wrapper">
-			
-
 			<div class="table-title">
 				<div class="row">
 					<div class="col">
@@ -136,12 +105,12 @@
 					</div>
 				</div>
 			</div>
-			
+
 			<table class="table table-striped table-hover">
 				<thead>
 					<tr>
 						<th>
-							
+
 						</th>
 						<th>Id</th>
 						<th>Name promotion</th>
@@ -153,7 +122,7 @@
 				<tbody>
 					<tr>
 						<td>
-							
+
 						</td>
 						<td>Thomas Hardy</td>
 						<td>thomashardy@mail.com</td>
@@ -166,24 +135,12 @@
 					</tr>
 				</tbody>
 			</table>
-			
+
 		</div>
 	</div>
 </div>
 
-{{-- footer --}}
-<footer class="text-center text-white fixed-bottom" style="background-color: #341366">
-	<!-- Grid container -->
-	<div class="container p-4"></div>
-	<!-- Grid container -->
-  
-	<!-- Copyright -->
-	<div class="text-center p-3" style="background-color: #351664(0, 0, 0, 0.2);">
-	  © 2022 Copyright:
-	  <a class="text-white" href="https://mdbootstrap.com/">solicode.com</a>
-	</div>
-	<!-- Copyright -->
-  </footer>
+
 </body>
 </html>
 
