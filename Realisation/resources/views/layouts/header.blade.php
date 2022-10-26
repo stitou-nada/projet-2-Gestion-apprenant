cb4a48716844a21ad446581a6cd17c3cb1be1aee
stitou-nada <nadastito1087@gmail.com>
2022-10-26 11:41:42 +0100

    header

--- Realisation/resources/views/layouts/header.blade.php
+++ Realisation/resources/views/layouts/header.blade.php
@@ -2,6 +2,4 @@
     <img class="logo" src="{{asset('img/logo.png')}}"  alt="">
     <h1 class="title-header">Gestion d'amprenant</h1>
 </div>
-<div class="devHeader">
-        <p class="devHeaderText"></p>
-</div>
+
