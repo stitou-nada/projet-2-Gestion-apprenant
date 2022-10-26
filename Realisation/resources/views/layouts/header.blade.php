156dcd2f995df589dde3a050353e4547276ffc93
stitou-nada <nadastito1087@gmail.com>
2022-10-26 11:19:20 +0100

    edit

--- Realisation/resources/views/layouts/header.blade.php
+++ Realisation/resources/views/layouts/header.blade.php
@@ -1,5 +1,5 @@
 <div class="header-box">
-    <img class="logo" src="{{asset('img/R.png')}}"  alt="">
+    <img class="logo" src="{{asset('img/logo.png')}}"  alt="">
     <h1 class="title-header">Gestion d'amprenant</h1>
 </div>
 <div class="devHeader">
